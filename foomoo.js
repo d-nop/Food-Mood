e1aab93571f05aab5c213bd08fe930040adf6a50
JBW <whitco670@jbw-mbp-215.attlocal.net>
2017-12-05 17:01:19 -0500

    click handler is throwing an ajax error.  commented out that partof code.  Need input from Lavelle and front-end.

--- foomoo.js
+++ foomoo.js
@@ -1,4 +1,4 @@
-
+console.log("hello foomoo");
 var authKey = "a5b4d2b8-921e-4930-832d-2eed566fce06";
 
 // These variables will hold the results we get from the user's inputs via HTML
@@ -61,44 +61,4 @@ $.ajax(settings).done(function (response) {
   let deliveryFee = response.fee/100;
   console.log("The delivery Fee is "+"$"+deliveryFee);
 });
-
-
-
-//$(document).on("click","input-delivery-submit", function(){
-  //  event.(preventDefault);
-
-   // let incoming_address = $("#input-delivery-text").val().trim();
-   // let outgoing_address = //need object call from Lavelle for Zomato
-
-  //  var form = new FormData();
-  //  form.append("dropoff_address", incoming_address);
-  //  form.append("pickup_address", outgoing_address);
-
-  //var settings = {
-  //  "async": true,
-  //  "crossDomain": true,
-  //  "url": "https://cors-anywhere.herokuapp.com/https://api.postmates.com/v1/customers/cus_LXPkZAcVyBksa-/delivery_quotes",
-  //  "method": "POST",
-  //  "headers": {
-  //  "Authorization": "Basic YTViNGQyYjgtOTIxZS00OTMwLTgzMmQtMmVlZDU2NmZjZTA2Og==",
-  //  "Cache-Control": "no-cache",
-  //  "Postman-Token": "fd32b0c3-4fbf-eacf-5604-b74f9fe30230"
- // },
- //   "processData": false,
-   // "contentType": false,
-   // "mimeType": "multipart/form-data",
-   // "data": form
-//}
-
-//$.ajax(settings).done(function (response) {
-//    response = JSON.parse(response);
-  //  console.log(response);
-  //  let deliveryFee = response.fee/100;
- //   $("#grabDivfromKaraandDan").text("The delivery Fee is "+"$"+deliveryFee);
-  //  console.log("The delivery Fee is "+"$"+deliveryFee);
-//});
-
-
-//});
-
 
