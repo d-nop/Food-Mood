9bbb9e97dc95e493c84bf43ad676e8de394969d6
JBW <whitco670@jbw-mbp-215.attlocal.net>
2017-12-06 08:17:28 -0500

    perform some general cleanup

--- assets/javascript/fm.js
+++ assets/javascript/fm.js
@@ -1,17 +1,10 @@
 
 var authKey = "a5b4d2b8-921e-4930-832d-2eed566fce06";
 
-// These variables will hold the results we get from the user's inputs via HTML
-var searchTerm = "";
-var numResults = 0;
-var startYear = 0;
-var endYear = 0;
 
-// queryURLBase is the start of our API endpoint. The searchTerm will be appended to this when
-// the user hits the search button
 var customer_id = "cus_LXPkZAcVyBksa-";
-var outgoing_address = "20 McAllister St, San Francisco, CA";
-var incoming_address = "101 Market St, San Francisco, CA";
+var outgoing_address = "9 palisades rd ne, atlanta, ga";
+var incoming_address = "999 peachtree st ne, atlanta, ga";
 var queryURLBase = "https://cors-anywhere.herokuapp.com/https://api.postmates.com/v1/customers/cus_LXPkZAcVyBksa-/delivery_quotes/"
 
 // Counter to keep track of article numbers as they come in
@@ -20,20 +13,7 @@ var articleCounter = 0;
 // FUNCTIONS
 // ==========================================================
 
-// This runQuery function expects two parameters:
-// (the number of articles to show and the final URL to download data from)
 
-  // The AJAX function uses the queryURL and GETS the JSON data associated with it.
-  // The data then gets stored in the variable called: "NYTData"
-
-//   $.ajax({
-//     url: queryURLBase,
-//     method: "POST"
-
-
-// }).done(function(response) {
-// 	conzole.log(response);
-// });
 
 var form = new FormData();
 form.append("dropoff_address", incoming_address);
