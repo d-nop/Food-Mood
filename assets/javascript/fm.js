04931f54b684c23f8b18093c708c986950662010
JBW <whitco670@lawn-128-61-9-195.lawn.gatech.edu>
2017-12-06 10:02:08 -0500

    ready to merge with LaVeileil

--- assets/javascript/fm.js
+++ assets/javascript/fm.js
@@ -7,8 +7,7 @@ var outgoing_address = "9 palisades rd ne, atlanta, ga";
 var incoming_address = "999 peachtree st ne, atlanta, ga";
 var queryURLBase = "https://cors-anywhere.herokuapp.com/https://api.postmates.com/v1/customers/cus_LXPkZAcVyBksa-/delivery_quotes/"
 
-// Counter to keep track of article numbers as they come in
-var articleCounter = 0;
+
 
 // FUNCTIONS
 // ==========================================================
@@ -44,41 +43,41 @@ $.ajax(settings).done(function (response) {
 
 
 
-//$(document).on("click","input-delivery-submit", function(){
-  //  event.(preventDefault);
+$(document).on("click","input-delivery-submit", function(){
+    event.(preventDefault);
 
-   // let incoming_address = $("#input-delivery-text").val().trim();
-   // let outgoing_address = //need object call from Lavelle for Zomato
+    let incoming_address = $("#input-delivery-text").val().trim();
+    let outgoing_address = //need object call from Lavelle for Zomato
 
-  //  var form = new FormData();
-  //  form.append("dropoff_address", incoming_address);
-  //  form.append("pickup_address", outgoing_address);
+    var form = new FormData();
+    form.append("dropoff_address", incoming_address);
+    form.append("pickup_address", outgoing_address);
 
-  //var settings = {
-  //  "async": true,
-  //  "crossDomain": true,
-  //  "url": "https://cors-anywhere.herokuapp.com/https://api.postmates.com/v1/customers/cus_LXPkZAcVyBksa-/delivery_quotes",
-  //  "method": "POST",
-  //  "headers": {
-  //  "Authorization": "Basic YTViNGQyYjgtOTIxZS00OTMwLTgzMmQtMmVlZDU2NmZjZTA2Og==",
-  //  "Cache-Control": "no-cache",
-  //  "Postman-Token": "fd32b0c3-4fbf-eacf-5604-b74f9fe30230"
- // },
- //   "processData": false,
-   // "contentType": false,
-   // "mimeType": "multipart/form-data",
-   // "data": form
-//}
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://cors-anywhere.herokuapp.com/https://api.postmates.com/v1/customers/cus_LXPkZAcVyBksa-/delivery_quotes",
+    "method": "POST",
+    "headers": {
+    "Authorization": "Basic YTViNGQyYjgtOTIxZS00OTMwLTgzMmQtMmVlZDU2NmZjZTA2Og==",
+    "Cache-Control": "no-cache",
+    "Postman-Token": "fd32b0c3-4fbf-eacf-5604-b74f9fe30230"
+  },
+    "processData": false,
+    "contentType": false,
+    "mimeType": "multipart/form-data",
+    "data": form
+}
 
-//$.ajax(settings).done(function (response) {
-//    response = JSON.parse(response);
-  //  console.log(response);
-  //  let deliveryFee = response.fee/100;
- //   $("#grabDivfromKaraandDan").text("The delivery Fee is "+"$"+deliveryFee);
-  //  console.log("The delivery Fee is "+"$"+deliveryFee);
-//});
+$.ajax(settings).done(function (response) {
+   response = JSON.parse(response);
+    console.log(response);
+    let deliveryFee = response.fee/100;
+    $("#grabDivfromKaraandDan").text("The delivery Fee is "+"$"+deliveryFee);
+    console.log("The delivery Fee is "+"$"+deliveryFee);
+});
 
 
-//});
+});
 
 
